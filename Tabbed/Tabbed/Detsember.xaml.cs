b0b4e61c256a73b6be1083579a58b23d3558cda1
opilane <opilane@HP-CZC6297B9K>
2020-10-07 12:28:45 +0300

    Add project files.

---
+++ Tabbed/Tabbed/Detsember.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tabbed
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Detsember : ContentPage
+    {
+        public Detsember()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Tabbed/Tabbed/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tabbed
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            Start_screen();
+            InitializeComponent();
+            
+        }
+        public async void Start_screen()
+        {
+            string result = await DisplayPromptAsync("Message", "Sisestage avatava kuu");
+            if (result == "Jaanuar")
+            {
+                
+            }
+            else if(result=="Veebruar")
+            {
+                
+            }
+            else if (result == "Marts")
+            {
+
+            }
+            else if (result == "Aprill")
+            {
+
+            }
+            else if (result == "Mai")
+            {
+
+            }
+            else if (result == "Juuni")
+            {
+
+            }
+            else if (result == "Juuli")
+            {
+
+            }
+            else if (result == "August")
+            {
+
+            }
+            else if (result == "September")
+            {
+
+            }
+            else if (result == "Oktoober")
+            {
+
+            }
+            else if (result == "November")
+            {
+
+            }
+            else if (result == "Detsember")
+            {
+
+            }
+        }
+    }
+}
